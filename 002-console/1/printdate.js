a7394c00cad41f3a3b6f9f3c0b83717da74545ad
Yury Belkin <yurabelkin@yandex.ru>
2026-02-07 20:12:45 +0300

    Домашнее задание 1.2 - версия 2

--- 002-console/1/printdate.js
+++ 002-console/1/printdate.js
@@ -3,31 +3,86 @@
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 
-yargs(hideBin(process.argv))
+const argv = yargs(hideBin(process.argv))
+    .option('year', {
+        alias: 'y',
+        type: 'integer',
+        description: 'Вывод текущего года либо сдвиг по году',
+    })
+    .option('month', {
+        alias: 'm',
+        type: 'integer',
+        description: 'Вывод текущего месяца либо сдвиг по месяцу',
+    })
+    .option('date', {
+        alias: 'd',
+        type: 'integer',
+        description: 'Вывод текущей даты(числа) либо сдвиг по дате(числу)',
+    })
     .command('current', 'текущая дата', (yargs) => {
         return yargs.option('year', { alias: 'y', type: 'boolean' })
             .option('month', { alias: 'm', type: 'boolean' })
             .option('date', { alias: 'd', type: 'boolean' })
     }, (argv) => {
-        console.log(argv)
-        date = new Date()
+        let date = new Date()
+
+        if (argv.year) {
+            console.log(date.getFullYear())
+        } else if (argv.month) {
+            console.log(date.getMonth() + 1)
+        } else if (argv.date) {
+            console.log(date.getDate())
+        } else {
+            console.log(date.toISOString())
+        }
     })
     .command('add', 'добавить время', (yargs) => {
         return yargs.option('year', { alias: 'y', type: 'number' })
             .option('month', { alias: 'm', type: 'number' })
             .option('date', { alias: 'd', type: 'number' })
     }, (argv) => {
-        date = new Date()
-        date.setFullYear(date.getFullYear() + argv.year, date.getMonth() + argv.month, date.getDate() + argv.date)
+        let date = new Date()
+
+        if (argv.year) {
+            date.setFullYear(date.getFullYear() + argv.year)
+        }
+        if (argv.month) {
+            date.setMonth(date.getMonth() + argv.month)
+        }
+        if (argv.date) {
+            date.setDate(date.getDate() + argv.date)
+        }
+        if (!argv.year && !argv.year && !argv.date) {
+            console.log('Не введены параметры сдвига времени вперед')
+        }
+        else {
+            console.log(date.toISOString())
+        }
+
     })
     .command('sub', 'отнять время', (yargs) => {
         return yargs.option('year', { alias: 'y', type: 'number' })
             .option('month', { alias: 'm', type: 'number' })
             .option('date', { alias: 'd', type: 'number' })
     }, (argv) => {
-        date.setFullYear(date.getFullYear() - argv.year, date.getMonth() - argv.month, date.getDate() - argv.date)
+        const date = new Date()
+
+        if (argv.year) {
+            date.setFullYear(date.getFullYear() - argv.year)
+        }
+        if (argv.month) {
+            date.setMonth(date.getMonth() - argv.month)
+        }
+        if (argv.date) {
+            date.setDate(date.getDate() - argv.date)
+        }
+
+        if (!argv.year && !argv.year && !argv.date) {
+            console.log('Не введены параметры сдвига времени назад')
+        }
+        else {
+            console.log(date.toISOString())
+        }
     })
-
-
-if (typeof date !== "undefined") console.log(date.toISOString())
-else console.log("Не введена корректная команда")
+    .demandCommand(1, 'Введите корректную команду')
+    .parse()
--- 002-console/2/guessnumber.js
+++ 002-console/2/guessnumber.js
@@ -1,13 +1,25 @@
+#!/usr/bin/env node
+'use strict'
 const readline = require('node:readline');
-num = Math.floor(Math.random() * 100)
+let num = Math.floor(Math.random() * 100)
 const { stdin: input, stdout: output } = require('node:process')
 const rl = readline.createInterface({ input, output })
 console.log('Загадано число в диапазоне от 0 до 100')
 
+rl.on('SIGINT', () => {
+    console.log('\nИгра завершена');
+    rl.close();
+});
+
 rl.on('line', (ans) => {
-    if (num > ans) console.log('Больше')
-    if (num < ans) console.log('Меньше')
-    if (ans == num) {
+    if (isNaN(ans) || parseInt(ans) < 0 || parseInt(ans) > 100) {
+        console.log('Пожалуйста, введите число от 0 до 100');
+        return;
+    }
+
+    if (num > parseInt(ans)) console.log('Больше')
+    if (num < parseInt(ans)) console.log('Меньше')
+    if (parseInt(ans) == num) {
         console.log('Отгадано число ' + String(num))
         rl.close()
     }
